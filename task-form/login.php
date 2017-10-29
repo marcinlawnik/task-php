1de049f05eb4854121c60e7f95c20a502ce10122
roertbb <robert.banaszak1997@gmail.com>
2017-10-29 22:08:19 +0100

    done task-form

--- task-form/login.php
+++ task-form/login.php
@@ -7,31 +7,45 @@
  */
 session_start();    // allows to use session variables
 
-/*
- * define $passwordHashed here
- */
+// define $passwordHashed here
+$passwordHashed = password_hash("test", PASSWORD_DEFAULT);
+
 define("USERNAME", "test"); // never store sensitive data like this, it's just to simplify code for our needs
 define("PASSWORD", $passwordHashed);    // it's a form of storing constant data in PHP
 
-/*
- * in this place, redirect user if he or she is already logged in
- */
+// in this place, redirect user if he or she is already logged in
+$loggedIn = false;
+if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) {
+    $loggedIn = true;
+}
+
+if ($loggedIn === true) {
+    header("Location: index.php");
+    exit();
+}
 
 // if user has used login form
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
     // getting data entered by user and shortening variables' names
     $usernameEntered = $_POST['username'];
-    /*
-     * create new variable for password here
-     */
+    // create new variable for password here
+    $passwordEntered = $_POST['password'];
+    // check if passwords match here
+    $isPasswordCorrect = password_verify($passwordEntered, constant("PASSWORD"));
+    
+    // here include what to do when data is correct or incorrect
+    if ($usernameEntered === constant("USERNAME") && $isPasswordCorrect) {
+        $_SESSION['loggedIn'] = true;
+        header("Location: index.php");
+        exit();
+    }
+    else {
+        if ($usernameEntered !== constant("USERNAME"))
+            $error = "Incorrect username!";
+        elseif (!$isPasswordCorrect)
+            $error = "Incorrect password!";
+    }
 
-    /*
-     * check if passwords match here
-     */
-
-    /*
-     * here include what to do when data is correct or incorrect
-     */
 }
 ?>
 <!DOCTYPE html>
